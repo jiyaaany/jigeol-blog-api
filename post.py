f26dc8ecf9e28775b71ed334acb68fc31d30a866
jiyaaany <jiyaaany@gmail.com>
2021-03-16 10:32:55 +0900

    post 모델 Api

--- post.py
+++ post.py
@@ -1,38 +1,71 @@
 from flask.globals import request
 from flask_restful import Resource
 from models import PostModel, db
-import pandas, json, datetime
+import pandas, json
+from datetime import datetime
 
 class Post(Resource):
   def get(self, post_idx):
-    query = db.session.query(PostModel).filter(PostModel.post_idx == post_idx)
-    pandas_query = pandas.read_sql(query.statement, query.session.bind)
-
-    post = json.loads(pandas_query.to_json(orient='records'))
-    print(post[0]['reg_date'])
-    print(post[0]['reg_date'].strftime('%Y-%m-%d %H:%M:%S'))
-    return post
+    post = db.session.query(PostModel).filter(PostModel.post_idx == post_idx).first()
+    
+    return {
+      'id': post.post_idx,
+      'title': post.title,
+      'content': post.content,
+      'reg_date': str(post.reg_date),
+      'user_idx': post.user_idx
+    }
 
   def put(self, post_idx):
-    data = request.get_json()
-    print(data['title'])
-    print(data['content'])
-    
-    return {"method": "put"}
+    if request.get_json():
+      post_data = request.get_json()
+      post = db.session.query(PostModel).filter(PostModel.post_idx == post_idx).first()
+
+      for key in list(post_data.keys()):
+        post[key] = post_data[key]
+
+      db.session.commit()
+
+    return {
+      'status': 200,
+      'message': '수정되었습니다.'
+    }
 
   def delete(self, post_idx):
-    return {"method": "delete"}
+    post = db.session.query(PostModel).filter(PostModel.post_idx == post_idx).first()
+    db.session.delete(post)
+    db.session.commit()
+      
+    return {
+      'status': 200,
+      'message': '삭제되었습니다.'
+    }
 
 class Posts(Resource):
   def post(self):
     try:
-      post_data = request.get_json()
-      post = PostModel(
-        post_data['title'],
-        post_data['content'],
-        post_data['user_idx']
-      )
-      db.session.add(post)
-      db.session.commit()
+      if request.get_json():
+        post_data = request.get_json()
+        post = PostModel(
+          post_data['title'],
+          post_data['content'],
+          post_data['user_idx']
+        )
+        db.session.add(post)
+        db.session.commit()
+
+        return {
+          'status': 200,
+          'message': '저장되었습니다.'
+        }
+      else:
+        query = db.session.query(PostModel)
+        pandas_query = pandas.read_sql(query.statement, query.session.bind)
+        posts = json.loads(pandas_query.to_json(orient='records'))
+
+        for post in posts:
+          post['reg_date'] = str(datetime.fromtimestamp(post['reg_date']/1000))
+
+        return posts
     except Exception as e:
       print(str(e))
